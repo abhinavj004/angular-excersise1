605e96ac7bd914a486db1c25eedce932e90924a4
abhinavj004 <abhinavjaiswal0144@gmail.com>
2021-02-02 10:51:50 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'uxtrendz';
+
+  
+  
+  
+  // for Users
+  users =[];
+  pushUsers(data){
+    this.users.push(data);
+  }
+  onRemoveUsers(item){
+    this.users.splice(item, 1)
+  }
+
+  // for Admins
+  admins =[];
+  pushAdmins(data){
+    this.admins.push(data);
+  }
+  onRemoveAdmins(item){
+    this.admins.splice(item, 1)
+  }
+
+  
+}
